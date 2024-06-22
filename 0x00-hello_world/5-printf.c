b6f9fb7f1b095c57a0877b288acff7adc46867df
Jiekwu <scholarmaryann@gmail.com>
2024-06-22 05:01:59 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main = this is the  main function
+*
+* Return: 0 when successful
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but he outcome is a piece of art,\n");
+	return (0);
+}
