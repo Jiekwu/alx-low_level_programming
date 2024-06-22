bbb21a3126784c2d416f633692695a80ff2a7c28
Jiekwu <scholarmaryann@gmail.com>
2024-06-22 05:25:18 -0700

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - this is the main functio
+*
+* Return: always 0
+*/
+
 int main(void)
 {
-	
+	printf("Prigramming is like building a multilingual puzzle");
+	return (0);
+}
