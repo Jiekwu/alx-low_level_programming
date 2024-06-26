991b02a747874cc9c36cccd092281f3d67870f79
Jiekwu <scholarmaryann@gmail.com>
2024-06-26 15:54:22 -0700

    FIVE

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
- * print_sign - prints sign
- * @n: function parameter
- * Retuetn: 1, 0 or -1
- */
+* print_sign - prints sign
+* @n: function parameter
+* Retuetn: 1, 0 or -1
+*/
 int print_sign(int n)
 {
 	if (n > 0)
