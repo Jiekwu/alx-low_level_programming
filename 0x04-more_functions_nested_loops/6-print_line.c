7763b4ac8dd867d790771b879a6d28c88ff21188
Jiekwu <scholarmaryann@gmail.com>
2024-06-27 14:08:47 -0700

    Yeah

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: number of times
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= 0; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
