ef75eb8e4a8a642b247dcb75737039919f1482b8
Jiekwu <scholarmaryann@gmail.com>
2024-07-01 18:18:33 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#iclude "main.h"
+
+/**
+ * _atoi
+ * @s: pointer
+ * Return: An integer
+ */
+
+int _atoi(char *s)
+
+{
+	int c = 0;
+	unsigned int n1 = 0;
+	int min = 1;
+	int isi = 0;
+
+	while (s[c])
+	{
+	if (s[c] == 45)
+	{
+	min += -1;
+	}
+	isi = 1;
+	ni
+	}
+}
