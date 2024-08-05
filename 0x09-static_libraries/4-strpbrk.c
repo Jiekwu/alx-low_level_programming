6de6b2dd3fa48e514b5911453f470f4b15a483d0
Jiekwu <scholarmaryann@gmail.com>
2024-08-05 01:30:35 -0700

    STATIC LIBRARY

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches for a set of bytes
+ * @s: string to search from
+ * @accept: string to search in s
+ * Return: accept or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		const char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+				return (s);
+			a++;
+		}
+		s++;
+	}
+
+	return (NULL);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - returns an absolute value
+ * @a: parameter
+ * Return: -a or a
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+}
